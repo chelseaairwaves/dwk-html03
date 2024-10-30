847f662ed1ea3ee680010faf4e9795153ec78548
chelseadewipertami <chelseadewipertami@gmail.com>
2024-10-30 01:40:42 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.title = 'chelsea airwaves'
+const body = document.body
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML = '<marquee>Watashi Monkey D Luffy</marquee>'
+body.append(namaSaya)
